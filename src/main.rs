4936624c11a24b12b0b398cef85a181fec439b82
Gremious <gremious@protonmail.com>
2019-12-28 07:10:40 +0000

    Move Reader to read file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::io::{BufWriter, Write};
+mod reader;
+mod writer;
 
 fn main() {
     let mainFile = File::open("src/files/textfile.txt").expect("Unable to open file");
-    println!("{}", readFile(mainFile));
-}
-
-fn readFile(input_file: File) -> String {
-    let tempFile: File = input_file;
-    let mut ourOutputText = String::new(); // Sting we'll return
-
-    let mut br = BufReader::new(tempFile);
-    br.read_to_string(&mut ourOutputText)
-        .expect("Unable to read string");
-    ourOutputText
+    println!("{}", reader::readFile(mainFile));
 }
---
+++ src/reader.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+pub(crate) fn readFile(input_file: File) -> String {
+    let tempFile: File = input_file;
+    let mut ourOutputText = String::new(); // Sting we'll return
+
+    let mut br = BufReader::new(tempFile);
+    br.read_to_string(&mut ourOutputText)
+        .expect("Unable to read string");
+    ourOutputText
+}
