3ac77a1ff34010cdffc3a401e6633cfc1d3d3f53
Gremious <gremious@protonmail.com>
2019-12-28 07:06:01 +0000

    Basic file reader.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::io::{BufWriter, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mainFile = File::open("src/textfile.txt").expect("Unable to open file");
+
+    println!("{}", readFile(mainFile));
+}
+
+fn readFile(input_file: File) -> String {
+    let tempFile: File = input_file;
+    let mut ourOutputText = String::new(); // Sting we'll return
+
+    let mut br = BufReader::new(tempFile);
+    br.read_to_string(&mut ourOutputText)
+        .expect("Unable to read string");
+    ourOutputText
 }
