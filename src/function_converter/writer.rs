2193b43503b94a44bf00042f693bd2454883cc46
Gremious <gremious@protonmail.com>
2019-12-31 06:25:53 +0000

    Setting up string handling

--- src/function_converter/writer.rs
+++ src/function_converter/writer.rs
@@ -3,7 +3,7 @@ use std::io::{BufReader, Read};
 use std::io::{BufWriter, Write};
 use crate::function_converter::file_helper;
 
-pub fn writeToFile(mut input_file: File, input_text: String){ggi
+pub fn writeToFile(mut input_file: File, input_text: String){
    input_file.write_all(input_text.as_bytes()).expect("Unable to write data");
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::fs;
 use std::fs::File;
+use std::io::Read;
 
 pub mod function_converter {
     pub mod file_helper;
@@ -7,8 +9,13 @@ pub mod function_converter {
 }
 
 fn main() {
-    let mainFile = File::open("src/files/textfile.txt").expect("Unable to open file");
-    let file2 = File::create("src/files/textfile2.txt").expect("Unable to open file");
-    println!("{}", function_converter::reader::read_file(mainFile));
+    let mut file1 = File::open("src/files/textfile.txt").expect("Unable to open file");
+    let mut file2 = File::create("src/files/textfile2.txt").expect("Unable to open file");
+    let mut file1Text = String::new();
+    let mut file2Text = String::new();
+
+    file1.read_to_string(&mut file1Text).expect("Unable to read the file");
+    println!("{}", file1Text);
+
     function_converter::writer::writeToFile(file2, "text".parse().unwrap());
 }
