87742dfecf8c9c89d0d84206795625c5cf6ed291
Gremious <gremious@protonmail.com>
2019-12-31 05:56:01 +0000

    Refactor folders a bit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 use std::fs::File;
 
 pub mod function_converter {
-    mod file_helper {
-        pub mod file_helper;
-    }
-    pub mod reader {
-        pub mod reader;
-    }
-    pub mod writer {
-        pub mod writer;
-    }
+    pub mod file_helper;
+    pub mod reader;
+    pub mod writer;
 }
 
 fn main() {
     let mainFile = File::open("src/files/textfile.txt").expect("Unable to open file");
     let file2 = File::create("src/files/textfile2.txt").expect("Unable to open file");
-    println!("{}", function_converter::reader::reader::read_file(mainFile));
-    function_converter::writer::writer::writeToFile(file2, "text".parse().unwrap());
+    println!("{}", function_converter::reader::read_file(mainFile));
+    function_converter::writer::writeToFile(file2, "text".parse().unwrap());
 }
