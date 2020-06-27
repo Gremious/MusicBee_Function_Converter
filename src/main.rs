3d266379aa2981f9b0009eb667505e47da0d6de7
Gremious Grenald <gremious@protonmail.com>
2020-06-27 16:46:13 +0100

    Declare curr dir

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,10 @@ pub mod reader;
 pub mod writer;
 
 fn main() -> std::io::Result<()> {
+    let curr_dir = env::current_dir()?;
     // Open the file that we're going to read, in read/append mode. Create it if it doesn't exist.
-    fs::create_dir_all(format!("{}/files/", env::current_dir()?.display()))?;
-    let path = format!("{}/files/mb_script.txt", env::current_dir()?.display());
+    fs::create_dir_all(format!("{}/files/", curr_dir.display()))?;
+    let path = format!("{}/files/mb_script.txt", curr_dir.display());
     let mut open_file = OpenOptions::new().append(true).create(true).read(true).open(&path)?;
 
     // We're going to copy the file contents into a buffer String
@@ -24,7 +25,7 @@ fn main() -> std::io::Result<()> {
 
     println!("Buffer: {}", buffer);
 
-    let output_file = File::create(format!("{}/files/mb_script_output.txt", env::current_dir()?.display()))
+    let output_file = File::create(format!("{}/files/mb_script_output.txt", curr_dir.display()))
         .expect("Unable to open file");
     let mut output_text = r#"$If(Criteria,True Result,False Result)"#.to_string();
     write_to_file(output_file, output_text);
