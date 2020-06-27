84a2859857c535342b1ceb379c765267e990e37b
Gremious Grenald <gremious@protonmail.com>
2020-06-27 12:51:12 +0100

    More cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
-mod function_converter;
-use crate::function_converter::reader::*;
-use crate::function_converter::writer::*;
-use std::fs;
-use std::fs::File;
-use std::io::Read;
+use crate::{reader::*, writer::*};
+use std::{fs::File, io::Read};
+pub mod reader;
+pub mod writer;
+
+extern crate rand;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+
+pub fn print_random_alphanum() -> String {
+    let outputString = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .collect::<String>();
+    outputString
+}
 
 fn main() {
-    let mut openFile    = File::open("src/files/textfile.txt").expect("Unable to open file");
+    let mut openFile = File::open("src/files/textfile.txt").expect("Unable to open file");
     let mut createdFile = File::create("src/files/textfile2.txt").expect("Unable to open file");
     let mut file1Text = String::new();
     let mut file2Text = String::new();
     file2Text = "This is a test string".parse().unwrap();
 
-    openFile.read_to_string(&mut file1Text).expect("Unable to read the file");
+    openFile
+        .read_to_string(&mut file1Text)
+        .expect("Unable to read the file");
     println!("{}", file1Text);
 
-    function_converter::writer::writeToFile(createdFile, file2Text);
+    writer::writeToFile(createdFile, file2Text);
 }
